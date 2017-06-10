8ce30485320de59bca2e82d275292576c3bb47c6
Rob Chiocchio <rob@mail.robeth.us>
2017-06-10 16:04:13 -0400

    Fixed errors with parsing inputs

--- scripts/yt-search.js
+++ scripts/yt-search.js
@@ -1,4 +1,6 @@
 //Searches for a song on youtube by its name and loads it into the MusicBot
+import 
+
 registerPlugin({
 	name: "Youtube Song Search",
 	version: "1.0",
@@ -15,6 +17,18 @@ registerPlugin({
 			title: "Search URL",
 			type: "string",
 			placeholder: "https://www.youtube.com/results?search_query="
+		},{
+			name: 'serverChatEnabled',
+			title: 'Listen in the serverchat',
+			type: 'checkbox'
+		},{
+			name: 'channelChatEnabled',
+			title: 'Listen in the channel chat',
+			type: 'checkbox'
+		},{
+			name: 'privateChatEnabled',
+			title: 'Listen in the private chat',
+			type: 'checkbox'
 		}
 	]
 }, function(sinusbot, config) {
@@ -36,18 +50,22 @@ registerPlugin({
 		
 		if (client.isSelf()) return;
 		
-		sinusbot.log("chat event: " + ev.text + ", " + ev.client.name() + ", " + ev.mode);
+		//engine.log("chat event: " + ev.text + ", " + ev.client.name() + ", " + ev.mode);
 		
 		if (ev.mode == 1 && config.privateChatEnabled ||
             ev.mode == 2 && config.channelChatEnabled ||
             ev.mode == 3 && config.serverChatEnabled) {
 
-			var message = ev.msg;
+			var commandRegex = new RegExp(/^!song(?: \w*?)??$/i) //allow custom commands
+			var messageRegex = new RegExp(\s(.*))
+			
+			var commandMatch = String(ev.msg).match(commandRegex);
+			var messageMatch = String(ev.msg).match(messageRegex);
 
-			if (message.substring(0,4) == config.commandTrigger) {
-				sinusbot.log("got command from " + ev.client.name());
+			if (commandMatch == config.commandTrigger) {
+				engine.log("got command from " + ev.client.name());
 				ev.client.chat("Attempting to queue requested song");
-				var request = encodeURI(message.substring(5));
+				var request = encodeURI(messageMatch);
 				var url = config.searchURL + request;
 				media.playURL(url);
 				return;
