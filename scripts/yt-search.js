c94893ff8d7eff8c7222f3fdd976812daf36e363
Rob Chiocchio <rob@mail.robeth.us>
2017-06-10 14:13:15 -0400

    Fixed formatting of yt-search.js

--- scripts/yt-search.js
+++ scripts/yt-search.js
@@ -1,23 +1,23 @@
 //Searches for a song on youtube by its name and loads it into the MusicBot
 registerPlugin({
-    name: "Youtube Song Search",
-    version: "1.0",
-    description: "Searches for songs on youtube",
-    author: "Rob Chiocchio",
-    vars: [
-        {
-            name: "command_trigger",
-            title: "Command",
-            type: "string",
-            placeholder: "play"
-        }
-    ]
+	name: "Youtube Song Search",
+	version: "1.0",
+	description: "Searches for songs on youtube",
+	author: "Rob Chiocchio",
+	vars: [
+		{
+			name: "command_trigger",
+			title: "Command",
+			type: "string",
+			placeholder: "play"
+		}
+	]
 },
 
 function(sinusbot, config) {
-    
-    var backend = require("backend");
-    var engine = require("engine");
-    var event = require("event");
-    
-    if 
+
+	var backend = require("backend");
+	var engine = require("engine");
+	var event = require("event");
+
+	if
