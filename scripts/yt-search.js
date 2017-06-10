0b180d16d5d005406055d82e9bd895bf19163ff9
Rob Chiocchio <RobethX@users.noreply.github.com>
2017-06-10 14:01:55 -0400

    Create yt-search.js

--- scripts/yt-search.js
+++ scripts/yt-search.js
@@ -1,1 +1,7 @@
 //Searches for a song on youtube by its name and loads it into the MusicBot
+registerPlugin({
+    name: "Youtube Song Search",
+    version: "1.0",
+    description: "Searches for songs on youtube",
+    author: "Rob Chiocchio",
+    vars: [
