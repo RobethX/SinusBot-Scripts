a2bf87935d06c6351bd3642851910dca2dd7d001
Rob Chiocchio <rob@mail.robeth.us>
2017-06-09 23:39:48 -0400

    Initialized spotify.js

---
+++ scripts/spotify.js
@@ -0,0 +1,14 @@
+registerPlugin({
+	name: 'Spotify',
+	version: '1.0',
+	description: 'idk',
+	author: 'Rob Chiocchio (robeth.us)'
+	vars: {
+		test: {
+			placeholder1: 'Hello, world!',
+			placeholder2: 'Hi mom!'
+		}
+	}
+}, functions(sinusbot, config, info) {
+	//stuff
+});
